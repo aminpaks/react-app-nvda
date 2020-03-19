0ab2d1fcf21c7376059cf94d042a452d58e1073a
Amin Pakseresht <amin.pakseresht@onespan.com>
2020-03-19 10:19:00 -0400

    Updates

---
+++ precache-manifest.34bb087efa6bc34537d89982f180d553.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6165b6a77ce5f397422173d232ad447b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0bdfd99d355368cd593",
+    "url": "/static/css/main.a2a2e0dd.chunk.css"
+  },
+  {
+    "revision": "1908dd69e983be5345df",
+    "url": "/static/js/2.9f99c939.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.9f99c939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0bdfd99d355368cd593",
+    "url": "/static/js/main.8bdb1da1.chunk.js"
+  },
+  {
+    "revision": "140d2e3fd67f8969b2b6",
+    "url": "/static/js/runtime-main.807c5b40.js"
+  }
+]);
